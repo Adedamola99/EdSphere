7d8fe33ce9145cc874d67284a03bcc7dc0573505
Damia <adebitandamola02@gmail.com>
2023-12-27 14:29:22 +0100

    incomplete selector

--- app/dashboard/grade/page.js
+++ app/dashboard/grade/page.js
@@ -1,7 +1,31 @@
+'use client'
+import { useState } from "react";
+import Dropdown from "@/components/Dropdown";
+
 const Grade = () => {
+    const options = [
+        { label: 'JSS1', value: 'JSS1' },
+        { label: 'JSS2', value: 'JSS2' },
+        { label: 'JSS3', value: 'JSS3' },
+    ]
+
+    const [value, setValue] = useState("fruit")
+
+    const handleChangeOption = (e) => {
+        setValue(e.target.value);
+    }
+
     return ( 
-        <div className="sub-container">
-            <h1>This is the user grade</h1>
+        <div className="grade-container">
+            <Dropdown 
+            label="Enter your school year"
+            options = {options}
+            value= {value}
+            onChange = {handleChangeOption}
+            />
+            <form>
+                
+            </form>
         </div>
      );
 }
